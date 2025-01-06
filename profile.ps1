464586a876462ae53e5fb656faf6950605a8193c
Deron Parker <>
2025-01-06 09:17:44 -0600

    init

---
+++ profile.ps1
@@ -0,0 +1,30 @@
+oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\onehalf.minimal.omp.json" | Invoke-Expression
+
+# make tab completion not bad
+Set-PSReadlineKeyHandler -Key Tab -Function Complete
+
+Function x { exit }
+
+Function gs { git status -u }
+Function gg { git log --oneline --graph }
+Function gd { git diff }
+Function gb { git branch -a }
+Function gbd { git branch -a | rg deron }
+
+Function ln ($target, $link) {
+    New-Item -Path $link -ItemType SymbolicLink -Value $target
+}
+
+Function cdln ($link) {
+    Get-Item $link | Select-Object -ExpandProperty Target | Set-Location
+}
+
+Function cd () {
+    Write-Output asdf
+}
+
+Function pwd2 () {
+    Write-Output (Get-Item (Get-Location).Path).FullName
+}
+
+$ENV:GIT_SSH="C:\Windows\System32\OpenSSH\ssh.exe"
